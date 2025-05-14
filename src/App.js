cb279ea6888ca9094a6fe63ca325242f7b2741d6
AhmedAlkasadi2003 <ahmed.alkasadi2003@gmail.com>
2025-05-14 12:32:02 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import Home from './pages/Home';
+import About from './pages/About';
+export default function App(){
+  return(
+
+    <Router>
+      <Routes>
+        <Route element ={<MainLayout/>}>
+        <Route path='/Home' element = {<Home/>}/>
+        <Route path='/About' element = {<About/>}/>
+
+        </Route>
+      </Routes>
+    </Router>
+  )
+}
