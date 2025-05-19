76bcca2f58655199703503c2f8cb30bc6151f0c6
AhmedAlkasadi2003 <ahmed.alkasadi2003@gmail.com>
2025-05-19 12:48:08 +0300

    commit7

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
 import About from './pages/About';
+export default function App(){
+  return(
 
-export default function App() {
-  return (
     <Router>
       <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
+        <Route element ={<MainLayout/>}>
+        <Route path='/Home' element = {<Home/>}/>
+
         </Route>
       </Routes>
     </Router>
-  );
+  )
 }
